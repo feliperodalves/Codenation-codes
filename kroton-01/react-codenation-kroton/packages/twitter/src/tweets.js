a1e5cf2e4aa615d9da3d03f418fc214db9553250
Felipe Alves <felipe.rod.alves@gmail.com>
2019-11-30 16:38:43 -0300

    feature: Challenge - Ricardo - Twitter JS

---
+++ kroton-01/react-codenation-kroton/packages/twitter/src/tweets.js
@@ -0,0 +1,41 @@
+import boy from '../imgs/boy.png';
+
+const tweets = [
+  {
+    author: 'Brie',
+    authorSrc: boy,
+    username: '@Sktch_ComedyFan',
+    elapsedTime: '3m',
+    message:
+      'Giving standup comedy a go. Open mic starts at 7, hit me up if you want ticket #heregoesnothing',
+    replies: 1,
+    retweets: 0,
+    likes: 8,
+    imgs: [],
+  },
+  {
+    author: 'Harold',
+    authorSrc: boy,
+    username: '@h_wang88',
+    elapsedTime: '10m',
+    message: 'Vacation is going great!',
+    replies: 3,
+    retweets: 5,
+    likes: 14,
+    imgs: ['../imgs/boruto.jpeg'],
+  },
+  {
+    author: 'Matheus',
+    authorSrc: boy,
+    username: '@Naomatheus',
+    elapsedTime: '30m',
+    message:
+      'Mano tem um gatinho preso miando faz horas e não da pra tirar ele do buraco :(',
+    replies: 1,
+    retweets: 0,
+    likes: 1,
+    imgs: [],
+  },
+];
+
+export default tweets;
