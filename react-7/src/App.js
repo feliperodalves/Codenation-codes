914912f82bf508b13127cca9dad0f96b3cda01af
feliperodalves <felipe.rod.alves@gmail.com>
2019-12-13 18:18:50 -0300

    feature: Weekend 5 challenge - initial commit - Get files from codenation - Running Create React App and get default Files - Add eslint / prettier configurations

---
+++ react-7/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oi</h1>
+    </div>
+  );
+}
+
+export default App;
