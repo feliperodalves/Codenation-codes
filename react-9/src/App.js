845acb6750ab455286d936d111842eaf2d2135df
Felipe Alves <felipe.rod.alves@gmail.com>
2020-01-15 10:36:18 -0300

    feature: Weekend 7 challenge - lint config
    - eslint / prettier / editorconfig configuration

--- react-9/src/App.js
+++ react-9/src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
-import Routes from "./routes";
+import Routes from './routes';
 
 function App() {
   return (
--- react-9/src/routes/index.js
+++ react-9/src/routes/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-import { Home } from "../pages/Home";
-import { About } from "../pages/About";
+import { Home } from '../pages/Home';
+import { About } from '../pages/About';
 
 const Routes = () => (
   <>
     <Switch>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/sobre" component={About}></Route>
-      <Route exact path="/404" render={() => <div>Not found!</div>}></Route>
-      <Route path="*" render={() => <Redirect to="/404" />}></Route>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/sobre" component={About} />
+      <Route exact path="/404" render={() => <div>Not found!</div>} />
+      <Route path="*" render={() => <Redirect to="/404" />} />
     </Switch>
   </>
 );
